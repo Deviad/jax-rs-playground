a5dcd6a22f175cd05632d72803037075ed85f57e
Davide Pugliese <dvd.pugliese@gmail.com>
2017-09-26 04:37:07 +0000

    first commit

---
+++ DemoApp.java
@@ -0,0 +1,12 @@
+import com.davidepugliese.jax.*;
+
+class DemoApp {
+    
+    public static void main(String [] args) {
+        //   TestClass test = new TestClass();
+        //   System.out.println(test.answer());
+        // HelloServlet servlet = new HelloServlet();
+        
+    }
+    
+}
